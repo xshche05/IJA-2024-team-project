8ec39478ace6958bf8a03a9b901c5b784532d47d
xshche05 <xshche05@stud.fit.vutbr.cz>
2024-04-28 13:26:51 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module ija.project.robot {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens ija.project.robot to javafx.fxml;
+    exports ija.project.robot;
+}
