b6795a56dfd1a9de3c07498b21f471bb6554bc5d
xshche05 <xshche05@stud.fit.vutbr.cz>
2024-05-01 19:40:00 +0200

    Changes

--- src/main/java/ija/project/robot/gui/controllers/Playground.java
+++ src/main/java/ija/project/robot/gui/controllers/Playground.java
@@ -4,13 +4,16 @@ import ija.project.robot.gui.interfaces.MenuInterface;
 import ija.project.robot.gui.interfaces.SceneInterface;
 import ija.project.robot.gui.logic.Menu;
 import ija.project.robot.logic.room.Room;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import static ija.project.robot.RobotApp.logger;
@@ -22,10 +25,10 @@ public class Playground implements MenuInterface, SceneInterface {
     public MenuItem MenuFileSaveAs;
     @FXML
     public MenuItem MenuFileLoad;
-    @FXML
-    public Canvas Canvas;
 
     private static final int gridWidth = 25;
+    @FXML
+    public HBox HBoxCanvas;
 
     @FXML
     public void initialize() {
@@ -57,13 +60,26 @@ public class Playground implements MenuInterface, SceneInterface {
         Room room = Room.getInstance();
         int width = room.getWidth();
         int height = room.getHeight();
+
+        Canvas Canvas = new Canvas();
+
         Canvas.setWidth(width * gridWidth);
         Canvas.setHeight(height * gridWidth);
         // Color canvas
         Canvas.getGraphicsContext2D().setFill(javafx.scene.paint.Color.RED);
         Canvas.getGraphicsContext2D().fillRect(0, 0, width * gridWidth, height * gridWidth);
-        AnchorPane.setMinHeight(height * gridWidth + 200);
-        AnchorPane.setMinWidth(width * gridWidth + 200);
+        AnchorPane.setMinHeight(height * gridWidth + 100);
+        AnchorPane.setMinWidth(width * gridWidth + 100);
+        HBoxCanvas.getChildren().add(Canvas);
+        Canvas.setOnMouseClicked(this::CanvasClicked);
+    }
+
+    public void CanvasClicked(MouseEvent e) {
+        logger.info("Canvas clicked");
+        logger.info("X: " + e.getX() + " Y: " + e.getY());
+        int x = (int) e.getX() / gridWidth;
+        int y = (int) e.getY() / gridWidth;
+        logger.info("X: " + x + " Y: " + y);
     }
 
     public static Scene getScene() {
